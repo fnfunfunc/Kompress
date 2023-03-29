4f0842cabb2ee9fd4afdb65752407c4b11174ce9
eternal <100830314+orientlegend@users.noreply.github.com>
2023-03-29 15:31:11 +0800

    Update README.md & Add license

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Kompress
+
+Only supports compression and decompression in the tar format. The test can be found
+in [TarTest.kt](src/commonTest/kotlin/archivers/tar/TarTest.kt)
+
+Some of the logical code is borrowed from [Apache-Common-Compress](https://github.com/apache/commons-compress)
--- src/commonMain/kotlin/archivers/tar/TarConstants.kt
+++ src/commonMain/kotlin/archivers/tar/TarConstants.kt
@@ -1,5 +1,3 @@
-package archivers.tar
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +16,9 @@ package archivers.tar
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package archivers.tar
+
 /**
  * This interface contains all the definitions used in the package.
  *
